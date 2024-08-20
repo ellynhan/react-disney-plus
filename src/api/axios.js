40311af86f53011800e78d282c3ae8cdce9a38a8
ellynhan <wown252@naver.com>
2024-08-21 02:53:59 +0900

    [feat] init files and axois request setting

---
+++ src/api/axios.js
@@ -0,0 +1,11 @@
+import axois from 'axios';
+
+const instance = axois.create({
+    baseURL: "https://api.themoviedb.org/3",
+    params: {
+        api_key: process.env.REACT_APP_MOVIE_DB_API_KEY,
+        language: "ko-KR"
+    }
+})
+
+export default instance;
