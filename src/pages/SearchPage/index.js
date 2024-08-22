73509da5652ff87fdc6d1522003f5bec9b355f6a
ellynhan <wown252@naver.com>
2024-08-22 17:36:44 +0900

    [feat] add usedebounce hooks for searching

--- src/pages/SearchPage/index.js
+++ src/pages/SearchPage/index.js
@@ -2,23 +2,26 @@ import React, { useState, useEffect} from 'react'
 import { useLocation, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import axios from '../../api/axios';
+import {useDebounce} from '../../hooks/useDebounce'
+import './SearchPage.css'
 
 const SearchPage = () => {
+  const navigate = useNavigate();
   const [searchResults, setSearchResults] = useState([]);
   
   const useQuery = () => {
       return new URLSearchParams(useLocation().search);
   }
-
+  
   let query = useQuery();
   const searchTerm = query.get("q");
-  const navigate = useNavigate();
-  console.log(searchTerm);
+  const debouncedSearchTerm = useDebounce(searchTerm, 500);
+
   useEffect(()=>{
-    if(searchTerm){
-      fetchSearchMovie(searchTerm)
+    if(debouncedSearchTerm){
+      fetchSearchMovie(debouncedSearchTerm)
     }
-  },[searchTerm])
+  },[debouncedSearchTerm])
 
   const fetchSearchMovie = async () => {
     try{
@@ -33,7 +36,7 @@ const SearchPage = () => {
   if(searchResults.length > 0){
     console.log(searchResults)
     return(
-      <section className='search-continaer'>
+      <section className='search-container'>
         {searchResults.map((movie) => {
           if(movie.backdrop_path !== null && movie.media_type !== "person"){
             const movieImageUrl = "https://image.tmdb.org/t/p/w500/" + movie.backdrop_path;
@@ -53,7 +56,7 @@ const SearchPage = () => {
       <section>
         <div className="no-results">
           <p className='no-results__text'>
-            찾고자 하는 검색어 "{searchTerm}"에 맞는 영화가 없습니다.
+            찾고자 하는 검색어 "{debouncedSearchTerm}"에 맞는 영화가 없습니다.
           </p>
         </div>
       </section>
